3cf352c2f6b27be332c4788dffcfd4ae02d19bc2
parthv15 <pvyas1512@gmail.com>
2025-11-09 17:42:04 +1100

    TSD-32 and TSD-18: protect dashboard route by mocking

--- src/app/dashboard/[uuid]/home/page.tsx
+++ src/app/dashboard/[uuid]/home/page.tsx
@@ -7,6 +7,8 @@ interface PortalPageProps {
 	params: Promise<{ uuid: string }>;
 }
 
+const ALLOWED_ORGS = ["7e14cd73-cff9-44ae-8463-ba7d6d4deb03"];
+
 const PortalPage = async ({ params }: PortalPageProps) => {
 	const session = await auth.api.getSession({
 		headers: await headers(),
@@ -17,6 +19,14 @@ const PortalPage = async ({ params }: PortalPageProps) => {
 	}
 
 	const slug = await params;
+
+	// need to update this to call the DB. This is mocked right now
+	const validOrgId = ALLOWED_ORGS.includes(slug.uuid);
+	if (!validOrgId) {
+		// this means that we have a session because we are logged in to some user Id
+		// but that doesn't mean we can just access any users dashboard
+		return <div>Unauthorized</div>;
+	}
 
 	return (
 		<>
