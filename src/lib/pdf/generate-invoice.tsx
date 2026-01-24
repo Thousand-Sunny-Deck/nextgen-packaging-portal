f6e016ae73ef8551bb34d635aba55e42d491c23e
parthv15 <pvyas1512@gmail.com>
2026-01-24 20:37:43 +0530

    type change

--- src/lib/pdf/generate-invoice.tsx
+++ src/lib/pdf/generate-invoice.tsx
@@ -1,6 +1,6 @@
 import { renderToBuffer } from "@react-pdf/renderer";
 import Invoice from "./template";
-import type { InvoiceData } from "./template";
+import { InvoiceData } from "./types";
 
 export async function generateInvoicePdf(invoiceData: InvoiceData) {
 	return await renderToBuffer(<Invoice invoiceData={invoiceData} />);
